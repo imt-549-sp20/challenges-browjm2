3683e088788e54bc3132eddc4d27851f39fc32bb
browjm2 <62731795+browjm2@users.noreply.github.com>
2020-05-18 16:36:01 -0700

    completed movie filtering

--- movies/js/app.js
+++ movies/js/app.js
@@ -2,6 +2,7 @@ function renderMovies(movie_list) {
   var tbody = document.querySelector("tbody"); //selecting the tbody element
   tbody.textContent = ""; //clearing any existing content in the body
   movie_list.forEach(function (movie) {
+    //for each movie
     tbody.appendChild(renderMovie(movie));
   });
 }
@@ -22,21 +23,39 @@ function renderMovie(movies) {
   return tr;
 }
 
-function renderMovieProp(value, nonNumeric) {
+function renderMovieProp(content, nonNumeric) {
   var td = document.createElement("td"); //Creating the new < td > element
-  td.textContent = value; //setting the td contents to the value parameter
+  td.textContent = content; //setting the td contents to the content parameter
   if (nonNumeric) {
     //If the values in < td > should be formatted as non-numeric....
     td.classList.add("non-numeric"); //add the css "non-numeric" style class
   }
   return td; //return the new < td > element to the caller
 }
+
 var filterInput = document.getElementById("movie-filter"); // Selects the filter form field where users can type
-filterInput.addEventListener("keyup", function filterMovies(MOVIES, query) {
-  //keyup event will run this function every time a key released
-  return MOVIES.filter(function(MOVIES.title)){
-    return MOVIES.title.toLowerCase().indexOf(query.toLowerCase()) !==-1
-  })
+//SHould this movie be in our list?
+function isMovieFound(movie) {
+  //Get the user input
+  var userInput = filterInput.value;
+  // Make the input lower case
+  var lowercaseUserInput = userInput.toLowerCase();
+  //make the movie title lower case so we can compare titles
+  var lowercaseTitle = movie.title.toLowerCase();
+  //check is the user input is in the lowercase movie title
+  if (lowercaseTitle.indexOf(lowercaseUserInput) >= 0) {
+    //see if the lowercase title contains the user input from the user input field.
+    return true; //if it matches we will return the movie in the results
+  } else {
+    return false; //if it does not match then we won't return the movie in the results
+  }
 }
+//Listen for when a user types into the filter field.
+filterInput.addEventListener("input", function () {
+  //find any movies that match the user input
+  var filtered_movies = MOVIES.filter(isMovieFound);
+  //update the movie table with the new list
+  renderMovies(filtered_movies);
+});
 
 renderMovies(MOVIES);
