7e759f917d25ba3d7f5a5a1bbd83bbd1700e80f4
browjm2 <62731795+browjm2@users.noreply.github.com>
2020-06-02 21:40:48 -0700

    Final Changes

--- seattle911/js/app.js
+++ seattle911/js/app.js
@@ -1,6 +1,6 @@
 // API endpoint for JSON response
 const seattle911API =
-  "https://data.seattle.gov/resource/grwu-wqtk.json?$where=datetime%20is%20not%20null&$order=datetime%20desc&$limit=50";
+  "https://data.seattle.gov/resource/grwu-wqtk.json?$where=datetime%20is%20not%20null&$order=datetime%20desc&$limit=50"; //Can change the limit to 500 (the "limit" is at the end of this url")
 
 // Create the map object, set the view and zoom
 const mymap = L.map("mapid").setView([47.604311, -122.331734], 11.5);
@@ -17,41 +17,38 @@ L.tileLayer(
       "pk.eyJ1Ijoibmlja2RlbmFyZGlzIiwiYSI6ImNqaGRla2pjMjBvYXgzNm13Yzc3aGIwM3kifQ.G2Tr-B7ppCNdj6xuM0Qc5A",
   }
 ).addTo(mymap);
-// Render the map on screen
-const dataLocation = []; //making my own array within an array for items that have lat and long
-var locationTotal = 0; //etting the variable of dataLocation to zero
-function renderMap(data) {
-  for (var i = 0; i < data.length; i++) {
-    dataLocation.push(data[i].latitude); //I thought this was pushing this data
-    dataLocation.push(data[i].longitude); //and this data into dataLocation
-    if (dataLocation.includes("", i)) {
-      //so that any sting inside of there counted
-      locationTotal++;
-    }
-  }
-  console.log(locationTotal); //gives me 0 - I don't understand why?
 
-  //then the goal was to take this new array and add markers which I have not idea how to do because the new array is empty.
-
-  // `data` is an array of objects
-  // Add each object to the map if `latitude` and `longitude` are available
-  // Use `bindPopup()` to add `type`, `datetime`, and `address` properties
-}
-// Fetch the API data source, parse JSON, render the map, and handle errors
-var dataSrc =
-  "https://data.seattle.gov/resource/grwu-wqtk.json?$where=datetime%20is%20not%20null&$order=datetime%20desc&$limit=50"; //creating a variable for the data source which is the url we want to pull data from
-var Map = [];
+//parse the JSON response
 function parseAsJson(response) {
-  //parse the JSON response
-  return response.json(); //return the JSON from the response
+  //return the JSON from the response
+  return response.json();
 }
-function callRenderMap(data) {
-  Map = data;
-  renderMap(data);
-}
+
+//What do to if there is an error
 function handleError(err) {
-  //What do to if there is an error
-  console.error(err); //show the error to the user
+  //show the error to the user
+  console.error(err);
   alert(err.message); //provides a "failed to fetch" error message
 }
-fetch(dataSrc).then(parseAsJson).then(callRenderMap).catch(handleError); //fetching data source, then parsing the JSON, then call RenderMaps, otherwise we are going to catch any errors by handling that error
+// Render the map on screen
+function renderMap(data) {
+  // `data` is an array of objects
+  // Add each object to the map if `latitude` and `longitude` are available
+  data.forEach(function (location) {
+    if (location.longitude !== undefined && location.latitude !== undefined) {
+      //this makes sure there is some value in longitude and some value in latitude before we start to add this to the map
+      var marker = L.marker([location.latitude, location.longitude]).addTo(
+        mymap
+      );
+      var fromnow = moment(location.datetime).fromNow();
+
+      // Use `bindPopup()` to add `type`, `datetime`, and `address` properties
+      marker.bindPopup(
+        "<b>" + location.type + "</b><br>" + fromnow + "<br>" + location.address
+      );
+    }
+  });
+}
+
+// Fetch the API data source, parse JSON, render the map, and handle errors
+fetch(seattle911API).then(parseAsJson).then(renderMap).catch(handleError); //Orders the page to run the following functions in order: fetching data source, then parsing the JSON, then call renderMap, otherwise we are going to catch any errors by handling that error
